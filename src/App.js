b126be6e2467f31057aae9e94d11a02047baa6ca
bunnysunny24 <75733731+bunnysunny24@users.noreply.github.com>
2024-09-28 17:17:45 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrendingRepos from './components/TrendingRepos'; // Import TrendingRepos component
+
+function App() {
+  return (
+    <div>
+      <TrendingRepos />
+    </div>
+  );
+}
+
+export default App;
